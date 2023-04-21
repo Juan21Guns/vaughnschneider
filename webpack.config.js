803aad48fae6c07c1dfce7bd9f5a9d5d95600954
Vaughn Schneider <reelyvaughn@gmail.com>
2023-04-21 10:27:53 -0400

    added working babel-loader, ready to start React

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIS_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: path.join(__dirname, '/client/src', 'index.js'),
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: path.join(__dirname, '/client/dist'),
+    path: DIS_DIR,
     filename: 'bundle.js'
+  },
+  devServer: {
+    static: DIS_DIR,
+    open: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+      test: /\.(jsx|js)$/,
+      include: SRC_DIR,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              "targets": "defaults"
+            }],
+            '@babel/preset-react'
+          ]
+        }
+      }]
+    }]
   }
 };
