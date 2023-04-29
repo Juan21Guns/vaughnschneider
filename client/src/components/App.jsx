ff5d867a4380be96c81bd84c3064b15f5c4d4e63
Vaughn Schneider <reelyvaughn@gmail.com>
2023-04-29 17:32:29 -0400

    added scroll listener

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,17 @@ import Header from './Header';
 import Main from './Main';
 
 function App() {
+  window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    const header = document.getElementById('sticky-bar');
+
+    if (scroll < 300) {
+      header.style.opacity = 0;
+    } else if (scroll >= 300) {
+      header.style.opacity = (1 - (400 / scroll)) * 3;
+    }
+  });
+
   return (
     <>
       <Header />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
+    <header id="sticky-bar">
       <div className="h-left">
         <h3>Home</h3>
         <h3>About</h3>
