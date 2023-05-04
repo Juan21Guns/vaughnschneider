fac3fb17e049867d29b7ead035e6da170fb1c8d5
Vaughn Schneider <reelyvaughn@gmail.com>
2023-05-03 22:32:02 -0400

    polished header

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 
 function Home() {
+  const scrollCallback = () => {
+    const header = document.getElementById('header');
+    if (window.scrollY < 300) {
+      header.style.transform = 'translateY(-200px)';
+      header.style.opacity = '0';
+    } else if (window.scrollY >= 300) {
+      header.style.opacity = '1';
+      header.style.transform = 'translateY(0)';
+    }
+  };
+
+  React.useEffect(() => {
+    const header = document.getElementById('header');
+    header.style.opacity = '0';
+    window.addEventListener('scroll', scrollCallback, true);
+
+    return () => {
+      window.removeEventListener('scroll', scrollCallback, true);
+    };
+  }, []);
+
   return (
     <h1>Welcome to my first website!</h1>
   );
--- client/src/components/pages/Nav.jsx
+++ client/src/components/pages/Nav.jsx
@@ -6,45 +6,65 @@ function Nav() {
   const x = useLocation();
   const distance = '-200px';
 
-  React.useEffect(() => {
+  const buttonFly = (hidden, option) => {
     const navBar = document.querySelector('.collapse');
     const button = document.querySelector('.box-collapse');
+
+    if (option === 'button') {
+      if (hidden) {
+        button.style.visibility = 'hidden';
+      }
+      navBar.style.transform = 'translateY(0)';
+      button.style.transform = `translateY(${distance})`;
+    } else if (option === 'header') {
+      button.style.visibility = 'visible';
+      navBar.style.transform = `translateY(${distance})`;
+      setTimeout(() => { button.style.transform = 'translateY(0)'; }, 1000);
+    }
+  };
+
+  React.useEffect(() => {
+    const listPath = document.querySelectorAll('ul a');
+    const path = document.getElementById(`${x.pathname}`);
+
+    listPath.forEach((element) => {
+      const nonMutate = element;
+      nonMutate.style.color = 'revert-layer';
+      nonMutate.style.textShadow = 'revert-layer';
+    });
+    path.style.color = 'rgb(255, 255, 255)';
+    path.style.textShadow = '0 2px 1px black';
+
     switch (x.pathname) {
       case '/':
-        button.style.visibility = 'hidden';
-        navBar.style.transform = 'translateY(0)';
-        button.style.transform = `translateY(${distance})`;
-
+        buttonFly(true, 'button');
         break;
       default:
-        button.style.visibility = 'visible';
-        navBar.style.transform = `translateY(${distance})`;
-        button.style.transform = 'translateY(0)';
+        buttonFly(false, 'header');
         break;
     }
-  }, [x]);
 
-  const clickHandler = (event) => {
-    event.preventDefault();
-    const button = document.querySelector('.box-collapse');
-    const navBar = document.querySelector('.collapse');
-    button.style.transform = `translateY(${distance})`;
-    navBar.style.transform = 'translateY(0)';
-  };
+    console.log(x.pathname);
+  }, [x]);
 
   return (
     <>
       <nav id="header">
         <div className="collapse">
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/activity">Activity</Link></li>
-            <li className="r-nav"><Link to="/contact">Contact</Link></li>
+            <li><Link to="/" id="/">Home</Link></li>
+            <li><Link to="/about" id="/about">About</Link></li>
+            <li><Link to="/projects" id="/projects">Projects</Link></li>
+            <li><Link to="/activity" id="/activity">Activity</Link></li>
+            <li className="r-nav"><Link to="/contact" id="/contact">Contact</Link></li>
           </ul>
+          <button type="button" onClick={() => { buttonFly(false, 'header'); }} className="up-arrow">
+            <i className="fa-solid fa-arrow-up" />
+          </button>
         </div>
-        <button type="button" onClick={clickHandler} className="box-collapse">Menu</button>
+        <button type="button" onClick={() => { buttonFly(false, 'button'); }} className="box-collapse">
+          <i className="fa-solid fa-bars" aria-label="Menu dropdown" />
+        </button>
       </nav>
       <Outlet />
     </>
