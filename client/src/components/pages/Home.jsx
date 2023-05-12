2754a6e3eefc467a08b0c30a60c5b56e42c4de1c
Vaughn Schneider <reelyvaughn@gmail.com>
2023-05-11 20:35:55 -0400

    didn't go with design

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 function Home() {
   const scrollCallback = () => {
+    const willChange = document.querySelector('.firstN');
+    const willChange2 = document.querySelector('.lastN');
     const header = document.getElementById('header');
     const rotateF = document.querySelector('.firstN');
     const rotateL = document.querySelector('.lastN');
@@ -10,23 +12,35 @@ function Home() {
     if (window.scrollY <= 300) {
       header.style.transform = 'translateY(-200px)';
       header.style.opacity = '0';
+      rotateF.style.transform = `translateX(${window.scrollY / 10}px)`;
+      rotateL.style.transform = `translateX(-${window.scrollY / 15}px)`;
     } else if (window.scrollY > 300) {
       header.style.opacity = '1';
       header.style.transform = 'translateY(0)';
     }
 
-    if (window.scrollY <= 350) {
-      rotateF.style.transform = `translateX(${window.scrollY / 10}px)`;
-      rotateL.style.transform = `translateX(-${window.scrollY / 15}px)`;
+    if (window.scrollY > 1000) {
+      willChange.style.willChange = 'revert-layer';
+      willChange2.style.willChange = 'revert-layer';
+    } else if (window.scrollY <= 999) {
+      willChange.style.willChange = 'transform';
+      willChange2.style.willChange = 'transform';
     }
   };
 
   React.useEffect(() => {
+    const willChange = document.querySelector('.firstN');
+    const willChange2 = document.querySelector('.lastN');
     const header = document.getElementById('header');
+
+    willChange.style.willChange = 'transform';
+    willChange2.style.willChange = 'transform';
     header.style.opacity = '0';
     window.addEventListener('scroll', scrollCallback, true);
 
     return () => {
+      willChange.style.willChange = 'revert-layer';
+      willChange2.style.willChange = 'revert-layer';
       window.removeEventListener('scroll', scrollCallback, true);
     };
   }, []);
@@ -38,6 +52,7 @@ function Home() {
         <div className="firstN"><h1>Vaughn</h1></div>
         <div className="lastN"><h1> Schneider</h1></div>
       </div>
+      <div className="bgImg" />
     </div>
   );
 }
